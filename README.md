3642a0888c8a79091e5fae86543de523690d3960
Mathis Antony <sveitser@gmail.com>
2024-09-18 12:35:39 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+### Installation
+
+Either `nix develop` or `direnv allow`.
+
+```
+cargo install cargo stylus
+cd foo
+cargo build
+cargo stylus check # will fail due to size
+```
---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        stableToolchain = pkgs.rust-bin.stable.latest.minimal.override {
+          extensions = [ "clippy" "llvm-tools-preview" "rust-src" ];
+          targets = [ "wasm32-unknown-unknown" ];
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            openssl
+            stableToolchain
+          ] ++ lib.optionals stdenv.isDarwin
+            [ darwin.apple_sdk.frameworks.SystemConfiguration ];
+
+          shellHook = ''
+            # Using a local cargo home to avoid polluting the user's cargo home
+            export CARGO_HOME=$PWD/.cargo
+            export PATH=$CARGO_HOME/bin:$PATH
+          '';
+        };
+      }
+    );
+}
