d50042db929707284e3e3c7ee052f886f38ed8f8
ImJeremyHe <yiliang.he@qq.com>
2024-09-30 09:54:36 +0800

    Add espresso-crypto-helper

---
+++ espresso-crypto-helper/src/lib.rs
@@ -0,0 +1,208 @@
+#![cfg_attr(not(feature = "export-abi"), no_main)]
+extern crate alloc;
+
+mod bytes;
+mod full_payload;
+mod hotshot_types;
+mod namespace_payload;
+mod sequencer_data_structures;
+mod uint_bytes;
+mod utils;
+
+use std::num::NonZeroU32;
+
+use ark_ff::PrimeField;
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use committable::{Commitment, Committable};
+use ethers_core::types::U256;
+use full_payload::{NsProof, NsTable};
+use getrandom::{register_custom_getrandom, Error};
+use hotshot_types::{VidCommitment, VidCommon};
+use jf_crhf::CRHF;
+use jf_merkle_tree::prelude::{
+    MerkleCommitment, MerkleNode, MerkleProof, MerkleTreeScheme, Sha3Node,
+};
+use jf_rescue::{crhf::VariableLengthRescueCRHF, RescueError};
+use sequencer_data_structures::{
+    field_to_u256, BlockMerkleCommitment, BlockMerkleTree, Header, Transaction,
+};
+use serde::Serialize;
+use sha2::{Digest, Sha256};
+use tagged_base64::TaggedBase64;
+
+use stylus_sdk::prelude::*;
+
+const MY_CUSTOM_ERROR_CODE: u32 = Error::CUSTOM_START + 42;
+pub fn always_fail(_buf: &mut [u8]) -> Result<(), Error> {
+    let code = NonZeroU32::new(MY_CUSTOM_ERROR_CODE).unwrap();
+    Err(Error::from(code))
+}
+
+register_custom_getrandom!(always_fail);
+
+#[storage]
+#[entrypoint]
+pub struct Entry {}
+
+#[public]
+impl Entry {
+    pub fn verify_namespace(
+        namespace: u64,
+        proof_bytes: Vec<u8>,
+        commit_bytes: Vec<u8>,
+        ns_table_bytes: Vec<u8>,
+        tx_comm_bytes: Vec<u8>,
+        common_data_bytes: Vec<u8>,
+    ) -> bool {
+        return verify_namespace_helper(
+            namespace,
+            &proof_bytes,
+            &commit_bytes,
+            &ns_table_bytes,
+            &tx_comm_bytes,
+            &common_data_bytes,
+        );
+    }
+
+    // pub fn verify_merkle_proof(
+    //     proof_bytes: Vec<u8>,
+    //     header_bytes: Vec<u8>,
+    //     block_comm_bytes: Vec<u8>,
+    //     circuit_block_bytes: Vec<u8>,
+    // ) -> bool {
+    //     return verify_merkle_proof_helper(
+    //         &proof_bytes,
+    //         &header_bytes,
+    //         &block_comm_bytes,
+    //         &circuit_block_bytes,
+    //     );
+    // }
+}
+
+#[derive(
+    Clone,
+    Copy,
+    Debug,
+    PartialEq,
+    Eq,
+    Hash,
+    Default,
+    CanonicalDeserialize,
+    CanonicalSerialize,
+    PartialOrd,
+    Ord,
+)]
+pub struct NamespaceId(u64);
+
+impl From<NamespaceId> for u32 {
+    fn from(value: NamespaceId) -> Self {
+        value.0 as Self
+    }
+}
+
+impl From<u32> for NamespaceId {
+    fn from(value: u32) -> Self {
+        Self(value as u64)
+    }
+}
+
+// pub type VidScheme = Advz<Bn254, sha2::Sha256>;
+pub type Proof = Vec<MerkleNode<Commitment<Header>, u64, Sha3Node>>;
+pub type CircuitField = ark_ed_on_bn254::Fq;
+
+// Helper function to verify a block merkle proof.
+//
+// proof_bytes: Byte representation of a block merkle proof.
+// root_bytes: Byte representation of a Sha3Node merkle root.
+// header_bytes: Byte representation of the HotShot header being validated as a Merkle leaf.
+// circuit_block_bytes: Circuit representation of the HotShot header commitment returned by the light client contract.
+// pub fn verify_merkle_proof_helper(
+//     proof_bytes: &[u8],
+//     header_bytes: &[u8],
+//     block_comm_bytes: &[u8],
+//     circuit_block_bytes: &[u8],
+// ) -> bool {
+//     let proof_str = std::str::from_utf8(proof_bytes).unwrap();
+//     let header_str = std::str::from_utf8(header_bytes).unwrap();
+//     let block_comm_str = std::str::from_utf8(block_comm_bytes).unwrap();
+//     let tagged = TaggedBase64::parse(&block_comm_str).unwrap();
+//     let block_comm: BlockMerkleCommitment = tagged.try_into().unwrap();
+
+//     let proof: Proof = serde_json::from_str(proof_str).unwrap();
+//     let header: Header = serde_json::from_str(header_str).unwrap();
+//     let header_comm: Commitment<Header> = header.commit();
+
+//     let proof = MerkleProof::new(header.height, proof.to_vec());
+//     let proved_comm = proof.elem().unwrap().clone();
+//     BlockMerkleTree::verify(block_comm.digest(), header.height, proof)
+//         .unwrap()
+//         .unwrap();
+
+//     let mut block_comm_root_bytes = vec![];
+//     block_comm
+//         .serialize_compressed(&mut block_comm_root_bytes)
+//         .unwrap();
+//     let field_bytes = hash_bytes_to_field(&block_comm_root_bytes).unwrap();
+//     let local_block_comm_u256 = field_to_u256(field_bytes);
+//     let circuit_block_comm_u256 = U256::from_little_endian(circuit_block_bytes);
+
+//     return proved_comm == header_comm && local_block_comm_u256 == circuit_block_comm_u256;
+// }
+
+// Helper function to verify a VID namespace proof that takes the byte representations of the proof,
+// namespace table, and commitment string.
+//
+// proof_bytes: Byte representation of a JSON NamespaceProof string.
+// commit_bytes: Byte representation of a TaggedBase64 payload commitment string.
+// ns_table_bytes: Raw bytes of the namespace table.
+// tx_comm_bytes: Byte representation of a hex encoded Sha256 digest that the transaction set commits to.
+pub fn verify_namespace_helper(
+    namespace: u64,
+    proof_bytes: &[u8],
+    commit_bytes: &[u8],
+    ns_table_bytes: &[u8],
+    tx_comm_bytes: &[u8],
+    common_data_bytes: &[u8],
+) -> bool {
+    // let proof_str = std::str::from_utf8(proof_bytes).unwrap();
+    let commit_str = std::str::from_utf8(commit_bytes).unwrap();
+    let txn_comm_str = std::str::from_utf8(tx_comm_bytes).unwrap();
+    // let common_data_str = std::str::from_utf8(common_data_bytes).unwrap();
+
+    let proof: NsProof = serde_json::from_slice(proof_bytes).unwrap();
+    let ns_table: NsTable = NsTable {
+        bytes: ns_table_bytes.to_vec(),
+    };
+    let tagged = TaggedBase64::parse(&commit_str).unwrap();
+    // let commit: VidCommitment = tagged.try_into().unwrap();
+    let commit: VidCommitment = VidCommitment::default();
+    let vid_common: VidCommon = serde_json::from_slice(common_data_bytes).unwrap();
+
+    let (txns, ns) = proof.verify(&ns_table, &commit, &vid_common).unwrap();
+
+    let namespace: u32 = namespace.try_into().unwrap();
+    let txns_comm = hash_txns(namespace, &txns);
+
+    return ns == namespace.into() && txns_comm == txn_comm_str;
+}
+
+// TODO: Use Commit trait: https://github.com/EspressoSystems/nitro-espresso-integration/issues/88
+fn hash_txns(namespace: u32, txns: &[Transaction]) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(namespace.to_le_bytes());
+    for txn in txns {
+        hasher.update(&txn.payload);
+    }
+    let hash_result = hasher.finalize();
+    format!("{:x}", hash_result)
+}
+
+// fn hash_bytes_to_field(bytes: &[u8]) -> Result<CircuitField, RescueError> {
+//     // make sure that `mod_order` won't happen.
+//     let bytes_len = ((<CircuitField as PrimeField>::MODULUS_BIT_SIZE + 7) / 8 - 1) as usize;
+//     let elem = bytes
+//         .chunks(bytes_len)
+//         .map(CircuitField::from_le_bytes_mod_order)
+//         .collect::<Vec<_>>();
+//     Ok(VariableLengthRescueCRHF::<_, 1>::evaluate(elem)?[0])
+// }
---
+++ espresso-crypto-helper/src/sequencer_data_structures.rs
@@ -0,0 +1,539 @@
+// TODO import from sequencer: https://github.com/EspressoSystems/nitro-espresso-integration/issues/87
+// This module is essentially copy and pasted VID logic from the sequencer repo. It is an unfortunate workaround
+// until the VID portion of the sequencer repo is WASM-compatible.
+use ark_ff::{BigInteger, PrimeField};
+use ark_serialize::{
+    CanonicalDeserialize, CanonicalSerialize, Compress, Read, SerializationError, Valid, Validate,
+};
+use bytesize::ByteSize;
+use committable::{Commitment, Committable, RawCommitmentBuilder};
+use derive_more::{Add, Display, From, Into, Sub};
+use digest::OutputSizeUser;
+use either::Either;
+use ethers_core::{
+    types::{Address, Signature, H256, U256},
+    utils::{parse_units, ParseUnits},
+};
+use jf_merkle_tree::{
+    prelude::{LightWeightSHA3MerkleTree, Sha3Digest, Sha3Node},
+    universal_merkle_tree::UniversalMerkleTree,
+    MerkleTreeScheme, ToTraversalPath,
+};
+use num_traits::PrimInt;
+use serde::{Deserialize, Serialize};
+use std::{default::Default, str::FromStr};
+use tagged_base64::tagged;
+use trait_set::trait_set;
+use typenum::Unsigned;
+
+use crate::{full_payload::NsTable, hotshot_types::VidCommitment};
+use crate::{
+    utils::{impl_serde_from_string_or_integer, Err, FromStringOrInteger},
+    NamespaceId,
+};
+
+trait_set! {
+    pub trait TableWordTraits = CanonicalSerialize
+        + CanonicalDeserialize
+        + TryFrom<usize>
+        + TryInto<usize>
+        + Default
+         + PrimInt
+        + std::marker::Sync;
+
+    // Note: this trait is not used yet as for now the Payload structs are only parametrized with the TableWord parameter.
+    pub trait OffsetTraits = CanonicalSerialize
+        + CanonicalDeserialize
+        + TryFrom<usize>
+        + TryInto<usize>
+        + Default
+        + std::marker::Sync;
+
+    // Note: this trait is not used yet as for now the Payload structs are only parametrized with the TableWord parameter.
+    pub trait NsIdTraits =CanonicalSerialize + CanonicalDeserialize + Default + std::marker::Sync;
+}
+
+pub struct Transaction {
+    pub namespace: NamespaceId,
+    pub payload: Vec<u8>,
+}
+
+impl Transaction {
+    pub fn new(namespace: NamespaceId, payload: Vec<u8>) -> Self {
+        Self { namespace, payload }
+    }
+}
+
+#[derive(Default, Hash, Copy, Clone, Debug, PartialEq, Eq, From, Into, Display)]
+#[display(fmt = "{_0}")]
+
+pub struct ChainId(U256);
+
+impl From<u64> for ChainId {
+    fn from(id: u64) -> Self {
+        Self(id.into())
+    }
+}
+
+impl FromStringOrInteger for ChainId {
+    type Binary = U256;
+    type Integer = u64;
+
+    fn from_binary(b: Self::Binary) -> Result<Self, Err> {
+        Ok(Self(b))
+    }
+
+    fn from_integer(i: Self::Integer) -> Result<Self, Err> {
+        Ok(i.into())
+    }
+
+    fn from_string(s: String) -> Result<Self, Err> {
+        if s.starts_with("0x") {
+            Ok(Self(U256::from_str(&s).unwrap()))
+        } else {
+            Ok(Self(U256::from_dec_str(&s).unwrap()))
+        }
+    }
+
+    fn to_binary(&self) -> Result<Self::Binary, Err> {
+        Ok(self.0)
+    }
+
+    fn to_string(&self) -> Result<String, Err> {
+        Ok(format!("{self}"))
+    }
+}
+
+macro_rules! impl_to_fixed_bytes {
+    ($struct_name:ident, $type:ty) => {
+        impl $struct_name {
+            pub(crate) fn to_fixed_bytes(self) -> [u8; core::mem::size_of::<$type>()] {
+                let mut bytes = [0u8; core::mem::size_of::<$type>()];
+                self.0.to_little_endian(&mut bytes);
+                bytes
+            }
+        }
+    };
+}
+
+impl_serde_from_string_or_integer!(ChainId);
+impl_to_fixed_bytes!(ChainId, U256);
+
+impl From<u16> for ChainId {
+    fn from(id: u16) -> Self {
+        Self(id.into())
+    }
+}
+
+#[derive(Hash, Copy, Clone, Debug, Default, Display, PartialEq, Eq, From, Into)]
+#[display(fmt = "{_0}")]
+pub struct BlockSize(u64);
+
+impl_serde_from_string_or_integer!(BlockSize);
+
+impl FromStringOrInteger for BlockSize {
+    type Binary = u64;
+    type Integer = u64;
+
+    fn from_binary(b: Self::Binary) -> Result<Self, Err> {
+        Ok(Self(b))
+    }
+
+    fn from_integer(i: Self::Integer) -> Result<Self, Err> {
+        Ok(Self(i))
+    }
+
+    fn from_string(s: String) -> Result<Self, Err> {
+        Ok(BlockSize(s.parse::<ByteSize>().unwrap().0))
+    }
+
+    fn to_binary(&self) -> Result<Self::Binary, Err> {
+        Ok(self.0)
+    }
+
+    fn to_string(&self) -> Result<String, Err> {
+        Ok(format!("{self}"))
+    }
+}
+
+/// Global variables for an Espresso blockchain.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct ChainConfig {
+    /// Espresso chain ID
+    chain_id: ChainId,
+    /// Maximum size in bytes of a block
+    max_block_size: BlockSize,
+    /// Minimum fee in WEI per byte of payload
+    base_fee: FeeAmount,
+    fee_contract: Option<Address>,
+    fee_recipient: FeeAccount,
+}
+
+impl Committable for ChainConfig {
+    fn tag() -> String {
+        "CHAIN_CONFIG".to_string()
+    }
+
+    fn commit(&self) -> Commitment<Self> {
+        let comm = committable::RawCommitmentBuilder::new(&Self::tag())
+            .fixed_size_field("chain_id", &self.chain_id.to_fixed_bytes())
+            .u64_field("max_block_size", self.max_block_size.0)
+            .fixed_size_field("base_fee", &self.base_fee.to_fixed_bytes())
+            .fixed_size_field("fee_recipient", &self.fee_recipient.to_fixed_bytes());
+        let comm = if let Some(addr) = self.fee_contract {
+            comm.u64_field("fee_contract", 1).fixed_size_bytes(&addr.0)
+        } else {
+            comm.u64_field("fee_contract", 0)
+        };
+        comm.finalize()
+    }
+}
+
+#[derive(Clone, Debug, Copy, PartialEq, Deserialize, Serialize, Eq, Hash)]
+pub struct ResolvableChainConfig {
+    chain_config: Either<ChainConfig, Commitment<ChainConfig>>,
+}
+
+impl ResolvableChainConfig {
+    pub fn commit(&self) -> Commitment<ChainConfig> {
+        match self.chain_config {
+            Either::Left(config) => config.commit(),
+            Either::Right(commitment) => commitment,
+        }
+    }
+    pub fn resolve(self) -> Option<ChainConfig> {
+        match self.chain_config {
+            Either::Left(config) => Some(config),
+            Either::Right(_) => None,
+        }
+    }
+}
+
+impl From<Commitment<ChainConfig>> for ResolvableChainConfig {
+    fn from(value: Commitment<ChainConfig>) -> Self {
+        Self {
+            chain_config: Either::Right(value),
+        }
+    }
+}
+
+impl From<ChainConfig> for ResolvableChainConfig {
+    fn from(value: ChainConfig) -> Self {
+        Self {
+            chain_config: Either::Left(value),
+        }
+    }
+}
+
+pub type BlockMerkleTree = LightWeightSHA3MerkleTree<Commitment<Header>>;
+pub type BlockMerkleCommitment = <BlockMerkleTree as MerkleTreeScheme>::Commitment;
+
+#[tagged("BUILDER_COMMITMENT")]
+#[derive(Clone, Debug, Hash, PartialEq, Eq, CanonicalSerialize, CanonicalDeserialize)]
+/// Commitment that builders use to sign block options.
+/// A thin wrapper around a Sha256 digest.
+pub struct BuilderCommitment(Sha256Digest);
+
+impl AsRef<Sha256Digest> for BuilderCommitment {
+    fn as_ref(&self) -> &Sha256Digest {
+        &self.0
+    }
+}
+
+// Header values
+#[derive(Clone, Debug, Deserialize, Serialize, Hash, PartialEq, Eq)]
+pub struct Header {
+    pub chain_config: ResolvableChainConfig,
+    pub height: u64,
+    pub timestamp: u64,
+    pub l1_head: u64,
+    pub l1_finalized: Option<L1BlockInfo>,
+    pub payload_commitment: VidCommitment,
+    pub builder_commitment: BuilderCommitment,
+    pub ns_table: NsTable,
+    pub block_merkle_tree_root: BlockMerkleCommitment,
+    pub fee_merkle_tree_root: FeeMerkleCommitment,
+    pub builder_signature: Option<Signature>,
+    pub fee_info: FeeInfo,
+}
+
+impl Committable for Header {
+    fn commit(&self) -> Commitment<Self> {
+        let mut bmt_bytes = vec![];
+        self.block_merkle_tree_root
+            .serialize_with_mode(&mut bmt_bytes, ark_serialize::Compress::Yes)
+            .unwrap();
+        let mut fmt_bytes = vec![];
+        self.fee_merkle_tree_root
+            .serialize_with_mode(&mut fmt_bytes, ark_serialize::Compress::Yes)
+            .unwrap();
+
+        RawCommitmentBuilder::new(&Self::tag())
+            .field("chain_config", self.chain_config.commit())
+            .u64_field("height", self.height)
+            .u64_field("timestamp", self.timestamp)
+            .u64_field("l1_head", self.l1_head)
+            .optional("l1_finalized", &self.l1_finalized)
+            .constant_str("payload_commitment")
+            .fixed_size_bytes(self.payload_commitment.as_ref().as_ref())
+            .constant_str("builder_commitment")
+            .fixed_size_bytes(self.builder_commitment.as_ref())
+            .field("ns_table", self.ns_table.commit())
+            .var_size_field("block_merkle_tree_root", &bmt_bytes)
+            .var_size_field("fee_merkle_tree_root", &fmt_bytes)
+            .field("fee_info", self.fee_info.commit())
+            .finalize()
+    }
+
+    fn tag() -> String {
+        // We use the tag "BLOCK" since blocks are identified by the hash of their header. This will
+        // thus be more intuitive to users than "HEADER".
+        "BLOCK".into()
+    }
+}
+
+pub type FeeMerkleTree = UniversalMerkleTree<FeeAmount, Sha3Digest, FeeAccount, 256, Sha3Node>;
+pub type FeeMerkleCommitment = <FeeMerkleTree as MerkleTreeScheme>::Commitment;
+
+/// Type alias for byte array of SHA256 digest length
+type Sha256Digest = [u8; <sha2::Sha256 as OutputSizeUser>::OutputSize::USIZE];
+
+#[derive(Default, Hash, Copy, Clone, Debug, PartialEq, Eq, Add, Sub, From, Into, Display)]
+#[display(fmt = "{_0}")]
+pub struct FeeAmount(U256);
+
+impl FromStringOrInteger for FeeAmount {
+    type Binary = U256;
+    type Integer = u64;
+
+    fn from_binary(b: Self::Binary) -> Result<Self, Err> {
+        Ok(Self(b))
+    }
+
+    fn from_integer(i: Self::Integer) -> Result<Self, Err> {
+        Ok(i.into())
+    }
+
+    fn from_string(s: String) -> Result<Self, Err> {
+        // For backwards compatibility, we have an ad hoc parser for WEI amounts represented as hex
+        // strings.
+        if let Some(s) = s.strip_prefix("0x") {
+            return Ok(Self(s.parse().unwrap()));
+        }
+
+        // Strip an optional non-numeric suffix, which will be interpreted as a unit.
+        let (base, unit) = s
+            .split_once(char::is_whitespace)
+            .unwrap_or((s.as_str(), "wei"));
+        match parse_units(base, unit).unwrap() {
+            ParseUnits::U256(n) => Ok(Self(n)),
+            ParseUnits::I256(_) => panic!("amount cannot be negative"),
+        }
+    }
+
+    fn to_binary(&self) -> Result<Self::Binary, Err> {
+        Ok(self.0)
+    }
+
+    fn to_string(&self) -> Result<String, Err> {
+        Ok(format!("{self}"))
+    }
+}
+
+impl_serde_from_string_or_integer!(FeeAmount);
+impl_to_fixed_bytes!(FeeAmount, U256);
+
+impl From<u64> for FeeAmount {
+    fn from(amt: u64) -> Self {
+        Self(amt.into())
+    }
+}
+
+impl CanonicalSerialize for FeeAmount {
+    fn serialize_with_mode<W: std::io::prelude::Write>(
+        &self,
+        mut writer: W,
+        _compress: Compress,
+    ) -> Result<(), SerializationError> {
+        Ok(writer.write_all(&self.to_fixed_bytes())?)
+    }
+
+    fn serialized_size(&self, _compress: Compress) -> usize {
+        core::mem::size_of::<U256>()
+    }
+}
+impl CanonicalDeserialize for FeeAmount {
+    fn deserialize_with_mode<R: Read>(
+        mut reader: R,
+        _compress: Compress,
+        _validate: Validate,
+    ) -> Result<Self, SerializationError> {
+        let mut bytes = [0u8; core::mem::size_of::<U256>()];
+        reader.read_exact(&mut bytes)?;
+        let value = U256::from_little_endian(&bytes);
+        Ok(Self(value))
+    }
+}
+
+impl CanonicalSerialize for FeeAccount {
+    fn serialize_with_mode<W: std::io::prelude::Write>(
+        &self,
+        mut writer: W,
+        _compress: Compress,
+    ) -> Result<(), SerializationError> {
+        Ok(writer.write_all(&self.0.to_fixed_bytes())?)
+    }
+
+    fn serialized_size(&self, _compress: Compress) -> usize {
+        core::mem::size_of::<Address>()
+    }
+}
+
+impl CanonicalDeserialize for FeeAccount {
+    fn deserialize_with_mode<R: Read>(
+        mut reader: R,
+        _compress: Compress,
+        _validate: Validate,
+    ) -> Result<Self, SerializationError> {
+        let mut bytes = [0u8; core::mem::size_of::<Address>()];
+        reader.read_exact(&mut bytes)?;
+        let value = Address::from_slice(&bytes);
+        Ok(Self(value))
+    }
+}
+
+#[derive(
+    Default,
+    Hash,
+    Copy,
+    Clone,
+    Debug,
+    Display,
+    Deserialize,
+    Serialize,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    From,
+    Into,
+)]
+#[display(fmt = "{_0:x}")]
+pub struct FeeAccount(Address);
+
+impl FeeAccount {
+    /// Return inner `Address`
+    pub fn address(&self) -> Address {
+        self.0
+    }
+    /// Return byte slice representation of inner `Address` type
+    pub fn as_bytes(&self) -> &[u8] {
+        self.0.as_bytes()
+    }
+    /// Return array containing underlying bytes of inner `Address` type
+    pub fn to_fixed_bytes(self) -> [u8; 20] {
+        self.0.to_fixed_bytes()
+    }
+}
+
+impl ToTraversalPath<256> for FeeAccount {
+    fn to_traversal_path(&self, height: usize) -> Vec<usize> {
+        self.0
+            .to_fixed_bytes()
+            .into_iter()
+            .take(height)
+            .map(|i| i as usize)
+            .collect()
+    }
+}
+
+impl Valid for FeeAmount {
+    fn check(&self) -> Result<(), SerializationError> {
+        Ok(())
+    }
+}
+
+impl Valid for FeeAccount {
+    fn check(&self) -> Result<(), SerializationError> {
+        Ok(())
+    }
+}
+
+#[derive(
+    Hash,
+    Copy,
+    Clone,
+    Debug,
+    Deserialize,
+    Serialize,
+    PartialEq,
+    Eq,
+    CanonicalSerialize,
+    CanonicalDeserialize,
+)]
+pub struct FeeInfo {
+    account: FeeAccount,
+    amount: FeeAmount,
+}
+
+#[derive(Clone, Copy, Debug, Default, Deserialize, Serialize, Hash, PartialEq, Eq)]
+pub struct L1BlockInfo {
+    pub number: u64,
+    pub timestamp: U256,
+    pub hash: H256,
+}
+
+impl Committable for L1BlockInfo {
+    fn commit(&self) -> Commitment<Self> {
+        let mut timestamp = [0u8; 32];
+        self.timestamp.to_little_endian(&mut timestamp);
+
+        RawCommitmentBuilder::new(&Self::tag())
+            .u64_field("number", self.number)
+            // `RawCommitmentBuilder` doesn't have a `u256_field` method, so we simulate it:
+            .constant_str("timestamp")
+            .fixed_size_bytes(&timestamp)
+            .constant_str("hash")
+            .fixed_size_bytes(&self.hash.0)
+            .finalize()
+    }
+
+    fn tag() -> String {
+        "L1BLOCK".into()
+    }
+}
+
+impl Committable for FeeInfo {
+    fn commit(&self) -> Commitment<Self> {
+        RawCommitmentBuilder::new(&Self::tag())
+            .fixed_size_field("account", &self.account.to_fixed_bytes())
+            .fixed_size_field("amount", &self.amount.to_fixed_bytes())
+            .finalize()
+    }
+    fn tag() -> String {
+        "FEE_INFO".into()
+    }
+}
+
+pub fn field_to_u256<F: PrimeField>(f: F) -> U256 {
+    if F::MODULUS_BIT_SIZE > 256 {
+        panic!("Shouldn't convert a >256-bit field to U256");
+    }
+    U256::from_little_endian(&f.into_bigint().to_bytes_le())
+}
+
+#[cfg(test)]
+mod tests {
+    use committable::Committable;
+
+    use super::Header;
+
+    #[test]
+    fn header_test() {
+        let header_str = include_str!("./mock_data/header.json");
+        let header = serde_json::from_str::<Header>(&header_str).unwrap();
+        // Copied from espresso sequencer reference test
+        let expected = "BLOCK~6Ol30XYkdKaNFXw0QAkcif18Lk8V8qkC4M81qTlwL707";
+        assert_eq!(header.commit().to_string(), expected);
+    }
+}
