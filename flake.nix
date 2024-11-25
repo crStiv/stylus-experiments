05c94e8202b9db4c9618a120dd92403dd8cb9919
crStiv <cryptostiv7@gmail.com>
2024-11-25 18:52:37 +0100

    fix flake.nix
    I understand it's small fix, but there was an unneccesary space

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
